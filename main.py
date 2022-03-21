27d9706a136e9978b5301002fef2fc6db679f807
amarhbaneen <amaralzana@gmail.com>
2022-03-21 19:43:10 +0200

    a new update

--- main.py
+++ main.py
@@ -1,19 +1,38 @@
-import datetime
+import math
+
 import pandas as pd
+from datetime import datetime
 
 class employee:
     def __init__(self, firstName, lastName, gender, birthDate, worBegin, salary,
                  law14Date, propertyValue, deposits, workLeave, propertyPayment, checkComplet, leaveReason):
+
         self.gender = gender
         self.firstName = firstName
         self.lastName = lastName
-        self.age = datetime.today().year - datetime.strptime(birthDate, '%d/%m/%Y').year
-        self.seniority = (datetime.strptime('31/12/2021', '%d/%m/%Y').year - datetime.strptime(worBegin, '%d/%m/%Y').year)/365.25
+        self.age = datetime.today().year - datetime.strptime(birthDate, '%Y-%m-%d').year
+        self.seniority = (datetime.strptime('2021-12-31', '%Y-%m-%d').year - datetime.strptime(worBegin, '%Y-%m-%d').year)/365.25
         self.salary = salary
-        self.law14date = datetime.strptime(law14Date, '%d/%m/%Y')
+        if type(law14Date) != float:
+            self.law14date = datetime.strptime(law14Date, '%Y-%m-%d')
+        else:
+            self.law14date = None
         self.propertyValue = propertyValue
         self.deposits = deposits
-        self.propertyNetWorth = propertyPayment + checkComplet
+        self.pp = propertyPayment
+        self.cc = checkComplet
+        if self.cc == 'nan':
+            self.cc = 0
+        else:
+            self.cc = float(self.cc)
+        if self.pp == 'nan':
+            self.pp = 0
+        else:
+            try:
+                self.pp = float(self.pp)
+            except:
+                self.pp = 0
+        self.propertyNetWorth = self.pp + self.cc
         self.leaveReason = leaveReason
 
 
@@ -24,6 +43,8 @@ df = df.reset_index()
 
 employeeArray = []
 for i in df.iloc:
-    employee = employee(i['שם'],i['שם משפחה'],i['מין'],i['תאריך לידה'],i['תאריך תחילת עבודה '],i['שכר '],
-                        i['תאריך  קבלת סעיף 14'],i['אחוז סעיף 14'],i['שווי נכס'],i['הפקדות'],
-                        i['תאריך עזיבה '],i['תשלום מהנכס'],i['השלמה בצ'ק])
+    e = employee(i['שם'],i['שם משפחה'],i['מין'],i['תאריך לידה'],i['תאריך תחילת עבודה '],i['שכר '],
+                        i['תאריך  קבלת סעיף 14'],i['שווי נכס'],i['הפקדות'],
+                        i['תאריך עזיבה '],i['תשלום מהנכס'],i["השלמה בצ'ק"],i['סיבת עזיבה'])
+    employeeArray.append(e)
+
