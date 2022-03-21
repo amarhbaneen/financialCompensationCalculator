cea1e8ecdaec2f8998aa503b60580bafe819e77a
amarhbaneen <amaralzana@gmail.com>
2022-03-21 18:12:35 +0200

    change the loop

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 import datetime
-import openpyxl
 import pandas as pd
-import openpyxl
-
-
 
 class employee:
     def __init__(self, firstName, lastName, gender, birthDate, worBegin, salary,
@@ -12,7 +8,7 @@ class employee:
         self.firstName = firstName
         self.lastName = lastName
         self.age = datetime.today().year - datetime.strptime(birthDate, '%d/%m/%Y').year
-        self.seniority = datetime.strptime(workLeave, '%d/%m/%Y').year - datetime.strptime(worBegin, '%d/%m/%Y').year
+        self.seniority = (datetime.strptime('31/12/2021', '%d/%m/%Y').year - datetime.strptime(worBegin, '%d/%m/%Y').year)/365.25
         self.salary = salary
         self.law14date = datetime.strptime(law14Date, '%d/%m/%Y')
         self.propertyValue = propertyValue
@@ -27,5 +23,7 @@ df = pd.read_csv('csvfile.csv', encoding='utf-8')
 df = df.reset_index()
 
 employeeArray = []
-for i in df.iterrows():
-    print(i)
+for i in df.iloc:
+    employee = employee(i['שם'],i['שם משפחה'],i['מין'],i['תאריך לידה'],i['תאריך תחילת עבודה '],i['שכר '],
+                        i['תאריך  קבלת סעיף 14'],i['אחוז סעיף 14'],i['שווי נכס'],i['הפקדות'],
+                        i['תאריך עזיבה '],i['תשלום מהנכס'],i['השלמה בצ'ק])
